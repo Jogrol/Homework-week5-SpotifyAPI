d1bba81c31825b2f43dd806ac64410e075118b50
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-09 14:07:58 +0100

    add retrieve all playlists by user

--- playlists/routes.js
+++ playlists/routes.js
@@ -1,43 +1,29 @@
 const {Router} = require('express')
-// const User = require('../users/model')
+const User = require('../users/model')
 const Playlist = require('./model')
+const auth = require('../auth/middleware')
 
 const router = new Router()
 
-router.get('/playlists', (req, res, next) => {
-    const limit = req.query.limit || 25
-    const offset = req.query.offset || 0
-  //Use promise all if you want to "fire" them together.
-    Promise.all([
-        Playlist.count(),
-        Playlist.findAll({ limit, offset })
-    ])
-    .then(playlists => { console.log(playlists)
-        res.send({ playlists})
-      })
-      .catch(error => next(error))
-})
-
-
-router.get('/playlists/:id', (req, res, next) => {
-    console.log(req)
+router.get('/playlists', auth, (req, res) => {
     Playlist
-    .findByPk(req.params.userId)
+    .findAll({ where: {userId : req.user.id}})
     .then(playlist => {
       if (!playlist) {
         return res.status(404).send({
-          message: `Company does not exist`
+          message: `"its a match"`
         })
       }
       return res.send(playlist)
     })
     .catch(error => next(error))
-})
+  })
 
-router.post('/playlists', (req, res, next) => {
+
+router.post('/playlists', auth, (req, res, next) => {
     Playlist
       .create(req.body)
-      .then(playlist => {
+      .then(playlist => { console.log(playlist)
         if (!playlist) {
           return res.status(404).send({
             message: `Playlist does not exist`
