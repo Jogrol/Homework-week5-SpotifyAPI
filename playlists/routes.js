d28b0f30a72592e95dc6fa5ea10db04d663f87d1
Joey Grolleman <joeygrolleman@gmail.com>
2019-02-09 14:57:08 +0100

    post playlist update

--- playlists/routes.js
+++ playlists/routes.js
@@ -2,8 +2,10 @@ const {Router} = require('express')
 const User = require('../users/model')
 const Playlist = require('./model')
 const auth = require('../auth/middleware')
-
 const router = new Router()
+
+//http :4000/playlists Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTU0OTcxNDMxOSwiZXhwIjoxNTQ5NzIxNTE5fQ.469cJiSndNYPt41A1_9KkiAeHaKm9vLAm3eG7WgQdgM"
+//htt POST http :4000/playlists Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTU0OTcxNDMxOSwiZXhwIjoxNTQ5NzIxNTE5fQ.469cJiSndNYPt41A1_9KkiAeHaKm9vLAm3eG7WgQdgM"
 
 router.get('/playlists', auth, (req, res) => {
     Playlist
@@ -19,19 +21,19 @@ router.get('/playlists', auth, (req, res) => {
     .catch(error => next(error))
   })
 
-
-router.post('/playlists', auth, (req, res, next) => {
+  router.post('/playlists', auth, (req, res, next) => {
+      console.log(req.user)
+        const playlist = {
+        name: req.body.name,
+        userId: req.user.id
+    }
     Playlist
-      .create(req.body)
-      .then(playlist => { console.log(playlist)
-        if (!playlist) {
-          return res.status(404).send({
-            message: `Playlist does not exist`
-          })
-        }
-        return res.status(201).send(playlist)
-      })
+      .create({playlist})
+      .then(playlist => {
+        return res.status(201).send(playlist)})
       .catch(error => next(error))
+    
   })
   
+
   module.exports = router
